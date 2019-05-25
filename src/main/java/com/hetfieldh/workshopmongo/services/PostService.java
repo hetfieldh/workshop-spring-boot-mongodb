ae613bc473b59c8b56acebe45340a0e6fc983e31
Douglas Osorio <douglas.anderson7@gmail.com>
2019-05-25 17:00:55 -0300

    Obtendo post por id

---
+++ src/main/java/com/hetfieldh/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.hetfieldh.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hetfieldh.workshopmongo.domain.Post;
+import com.hetfieldh.workshopmongo.repository.PostRepository;
+import com.hetfieldh.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto nao encontrado");
+		}
+		return user;
+	}
+}
